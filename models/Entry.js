9423a86113f4579162f49e910589deb31eead2a6
velproject2 <velproject2@gmail.com>
2025-04-09 09:23:05 +0530

    Update Entry.js

--- models/Entry.js
+++ models/Entry.js
@@ -1,7 +1,7 @@
 async function initialize(client) {
   try {
     await client.execute(
-      'CREATE TABLE IF NOT EXISTS test_entries (apxNumber text, modelName text, track text, trackNumber text, driverName text, email text, checkInTime text, checkOutTime text, totalPrice double, PRIMARY KEY (apxNumber, checkInTime))'
+      'CREATE TABLE IF NOT EXISTS test_entries (apxNumber text, modelName text, track text, trackNumber text, vehicleWeight text, driverName text, email text, checkInTime text, checkOutTime text, totalPrice double, PRIMARY KEY (apxNumber, checkInTime))'
     );
     console.log('test_entries table initialized');
   } catch (err) {
